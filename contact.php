30cb2973138af4d3ff3543fcb0fb1e172484cfb0
Diogo Barreto <diogob1789@gmail.com>
2018-02-17 02:56:08 -0200

    include footer e include header

--- contact.php
+++ contact.php
@@ -33,22 +33,7 @@
 <body>
 	<div id = "container">
 	
-	<header>
-	
-	<div class="centro" style="position:relative;">
-	<div class="animacao"></div> 
-		<div id="logo"><a href="home.php"><img src ="logo.jpg" class="tamanhologo" alt="Grêmio logo"></a></div>
-		<div class ="titulo"><h1> Grêmio USA </h1></div>
-		
-			<div class = "menusocial">
-			<a href="https://www.facebook.com/GREMIOUSA/?fref=ts" target="_blank"><img id="facebook" src ="facebook.png" alt="Facebook botão" style="height:35px"></a>
-			<a href="https://twitter.com/gremio_usa" target="_blank"><img id="twitter" src ="twitter.png" alt="Twitter botão" style="height:35px"></a>
-			</div>
-			
-	</div>
-	
-	
-	</header>
+	<?php include 'header.php'; ?>
 
 	<!--apenas o location.html sem include nav-->
 	<?php include 'nav.php'; ?>
@@ -62,7 +47,6 @@
 	
 	</section>
 	
-
 	<?php include 'footer.php'; ?>
 	
 	</div>
--- news6.php
+++ news6.php
@@ -33,22 +33,7 @@
 <body>
 	<div id = "container">
 	
-	<header>
-	
-	<div class="centro" style="position:relative;">
-	<div class="animacao"></div> 
-		<div id="logo"><a href="home.php"><img src ="logo.jpg" class="tamanhologo" alt="Grêmio logo"></a></div>
-		<div class ="titulo"><h1> Grêmio USA </h1></div>
-		
-			<div class = "menusocial">
-			<a href="https://www.facebook.com/GREMIOUSA/?fref=ts" target="_blank"><img id="facebook" src ="facebook.png" alt="Facebook botão" style="height:35px"></a>
-			<a href="https://twitter.com/gremio_usa" target="_blank"><img id="twitter" src ="twitter.png" alt="Twitter botão" style="height:35px"></a>
-			</div>
-			
-	</div>
-	
-	
-	</header>
+	<?php include 'header.php'; ?>
 
 	<!--apenas o location.html sem include nav-->
 <?php include 'nav.php'; ?>
@@ -64,25 +49,8 @@
 	
 	
 	</section>
-	<footer>
-		<div class="centro">
-		<p class="pendereco"> Sponsors:</p>
-		<ul>
-		<li><a href="http://www.classicpizza1.com/" target="_blank"><img src="logop1.png" id="logop1"></a></li>
-		<li><a href="http://www.chevrolet.com/" target="_blank"><img src="logop3.jpg" id="logop3"></a></li>
-		<li><a href="http://jumpinwithus.net/" target="_blank"><img src="logop2.png" id="logop2"></a></li>
-		</ul>
-		</div>
-		
-		<div class="endereco">
-
-		<p class="pendereco">&reg 2015 Grêmio USA | Hudson Portuguese Club: 13 Port St, Hudson, MA 01749, USA | 
-33 Union Ave, Sudbury, MA 01776, USA - Contact number: (508) 9339124. &copy Grêmio USA - All rights reserved.</p>
-		<p class="pendereco">Developed by: &copy <a href="https://br.linkedin.com/pub/diogo-barreto/58/b09/949" target="_blank;" style="color:#fff"> Diogo Barreto</a>
-		
-		</div>
-		
-	</footer>
+	
+	<?php include 'footer.php'; ?>
 	
 	</div>
 </body>
