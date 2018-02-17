0fc876dbde2e7416f4f110dff2b4daea3cae2e34
Diogo Barreto <diogob1789@gmail.com>
2018-02-16 22:15:39 -0200

    meu primeiro commit

---
+++ nav.php
@@ -0,0 +1,13 @@
+	<nav>
+	<div class="centrosp">
+		<ul>
+		<li><a href ="home.php">Home</a></li>
+		<li><a href ="registration.php">Registration</a></li>
+		<li><a href ="news.php">News</a></li>
+		<li><a href ="pictures.php">Pictures</a></li>
+		<li><a href ="locationmass.php">Location</a></li>
+		<li><a href ="contact.php">Contact</a></li>
+		</ul>
+	</div>
+		
+	</nav>
---
+++ registration.php
@@ -0,0 +1,252 @@
+<!doctype html>
+<html>
+<head>
+<link href="favicon.ico" rel="shortcut icon" type="image/x-icon">
+<meta charset="utf-8">
+<meta name ="keyword" content="gremio, gremio usa, futebol, soccer, clube gremio, gremio estados unidos, escolhinha">
+<meta name ="description" content="Escolhinha de Futebol Oficial do Grêmio USA. Futebol do Grêmio agora em solo americano."> 
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Grêmio USA - Official Grêmio FBPA Soccer Training in USA </title>
+<link href='https://fonts.googleapis.com/css?family=Fjalla+One' rel='stylesheet' type='text/css'>
+<link href='gremio.css' rel='stylesheet' type='text/css'/>
+<style>
+/* Chrome, Safari, Opera*/ 
+@-webkit-keyframes example {
+    0%   {background: url(fotoheader1.jpg) 0px 0px no-repeat;}
+    25%  {background: url(fotoheader2.jpg) 0px 0px no-repeat;}
+    50%  {background: url(fotoheader3.jpg) 0px 0px no-repeat;}
+    75%  {background: url(fotoheader4.jpg) 0px 0px no-repeat;}
+	100% {background: url(fotoheader6.jpg) 0px 0px no-repeat;}
+}
+
+/* Standard syntax */ 
+@keyframes example {
+    0%   {background: url(fotoheader1.jpg) 0px 0px no-repeat;}
+    25%  {background: url(fotoheader2.jpg) 0px 0px no-repeat;}
+    50%  {background: url(fotoheader3.jpg) 0px 0px no-repeat;}
+	75%  {background: url(fotoheader4.jpg) 0px 0px no-repeat;}
+    100% {background: url(fotoheader6.jpg) 0px 0px no-repeat;}
+}
+@-webkit-keyframes example2 {
+    0%   {background: url(banner1.jpg) 0px 0px no-repeat;}
+    33%  {background: url(banner2.jpg) 0px 0px no-repeat;}
+    66%  {background: url(banner3.jpg) 0px 0px no-repeat;}
+    100%  {background: url(banner4.jpg) 0px 0px no-repeat;}
+}
+
+/* Standard syntax */ 
+@keyframes example2 {
+    0%   {background: url(banner1.jpg) 0px 0px no-repeat;}
+    33%  {background: url(banner2.jpg) 0px 0px no-repeat;}
+    66%  {background: url(banner3.jpg) 0px 0px no-repeat;}
+    100%  {background: url(banner4.jpg) 0px 0px no-repeat;}
+}
+</style>
+</head>
+<body>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-85020168-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+	<div id = "container">
+	
+<header>
+	
+	<div class="centro" style="position:relative;">
+	<div class="animacao"></div> 
+		<div id="logo"><a href="home.html"><img src ="logo.jpg" class="tamanhologo" alt="Grêmio logo"></a></div>
+		<div class ="titulo"><h1> Grêmio USA </h1></div>
+		
+			<div class = "menusocial">
+			<a href="https://www.facebook.com/GREMIOUSA/?fref=ts" target="_blank"><img id="facebook" src ="facebook.png" alt="Facebook botão" style="height:35px"></a>
+			<a href="https://twitter.com/gremio_usa" target="_blank"><img id="twitter" src ="twitter.png" alt="Twitter botão" style="height:35px"></a>
+			</div>
+			
+	</div>
+	
+	
+	</header>
+
+	<!--apenas o location.html sem include nav-->
+<?php include 'nav.php'; ?>
+	
+	<section class = "centro">
+	
+	
+<h2>SCHEDULE</h2>
+
+<div style="clear:both; height:330px;">
+<article class= "aregistration" style="margin-left:15px;">
+
+<img src ="boy gremio.gif" style="float:left; padding-right:9px;"> 
+
+<h3 style="padding-top:15px;"> BOYS: </h3>
+
+<p style="padding-bottom:10px;"><b>MONDAY -</b> 6PM TO 8PM<br>
+ 
+<b>THURSDAY -</b> 6PM TO 8PM</p>
+
+
+<b><h5>PRICE:</h5></b>
+<p style="padding-bottom:20px; padding-top:0px;"><b>1x WEEKLY</b> - $60.00 MONTH + $40.00 TRAINING UNIFORM.<br>
+<b>2x WEEKLY</b> - $100.00 MONTH + $40.00 TRAINING UNIFORM.<br>
+<b>1x WEEKLY 2 MONTHS</b> $100 + $40 TRAINING UNIFORM.<br>
+<b>2x WEEKLY 2 MONTHS</b> $180.00 + $40 TRAINING UNIFORM.<br>
+</p>
+
+</article>
+
+
+
+<article class= "aregistration">
+
+<img src ="girl gremio.jpg" style="float:left; width:179px; height:274px; padding-right:9px;">
+
+<h3 style="padding-top:15px;"> GIRLS: </h3>
+<p style="padding-bottom:10px;"><b>FRIDAY -</b> 5 TO 8 YEARS  6PM TO 7PM</p>
+
+<b><h5>PRICE:</h5></b>
+<p style="padding-bottom:39px; padding-top:0px;"><b>1x WEEKLY</b> - $60.00 MONTH + $40.00 TRAINING UNIFORM.<br>
+<b>2x WEEKLY</b> - $100.00 MONTH + $40.00 TRAINING UNIFORM.<br>
+<b>1x WEEKLY 2 MONTHS</b> $100 + $40 TRAINING UNIFORM.<br>
+<b>2x WEEKLY 2 MONTHS</b> $180.00 + $40 TRAINING UNIFORM.<br>
+</p>
+
+
+</article>
+</div>
+
+<h2> YOU CAN DO YOUR REGISTRATION HERE </h2>
+<h2 style="font-size:22px; padding-top:15px;">PAY NOW</h2>
+
+
+<!-- U$100,00 -->
+<div class="divpaypal">
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" style="padding-left:130px;">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="6TVVCGTEJDQWC">
+
+
+<table style="padding-top:10px;">
+<tr style="font-size:22px; font-family:'Fjalla One', sans-serif;">
+<td>
+<input type="hidden" name="on0" value="once weekly,  4 weeks + unif  U$ 100.00">Once weekly,  
+	1 month + unif  U$ 100.00
+</td>
+</tr>
+	<tr style="text-align:center;">
+	<td style="padding-top:5px;">
+	<select name="os0" style="border: 1px solid #00adf1;">
+	<option value="Once weekly,  4 weeks + unif">Once weekly,  4 weeks + unif </option>
+	</select> 	
+	</td>
+	</tr>
+</table>
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" height="60px" width="200px" style="padding: 5px 70px 0px 70px;">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+</div>
+
+
+<!-- U$140,00 -->
+<div class="divpaypal" style="margin-top:50px;">
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" style="padding-left:20px;">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="V2FV83GYPDESY">
+
+
+<table style="padding-top:10px;">
+<tr style="font-size:22px; font-family:'Fjalla One', sans-serif;">
+<td>
+<input type="hidden" name="on0" value="twice weekly,  4 weeks + unif  U$ 140.00">
+	Once weekly 2 months or twice weekly 1 month + unif U$ 140.00
+</td>
+</tr>
+	<tr style="text-align:center;">
+	<td style="padding-top:5px;">
+	<select name="os0" style="border: 1px solid #00adf1;">
+	<option value="twice weekly,  4 weeks + unif">Twice weekly,  4 weeks + unif </option>
+	</select> 	
+	</td>
+	</tr>
+</table>
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" height="60px" width="200px" style="padding: 5px 180px 0px 180px;">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+</div>
+
+
+<!-- U$220,00 -->
+<div class="divpaypal" style="margin-top:50px; margin-bottom:50px;">
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" style="padding-left:120px;">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="2SD9DQV2KYVR2">
+
+
+<table style="padding-top:10px;">
+<tr style="font-size:22px; font-family:'Fjalla One', sans-serif;">
+<td>
+<input type="hidden" name="on0" value="twice weekly,  8 weeks + unif  U$ 220.00">Twice weekly  
+	2 months + unif  U$ 220.00
+</td>
+</tr>
+	<tr style="text-align:center;">
+	<td style="padding-top:5px;">
+	<select name="os0" style="border: 1px solid #00adf1;">
+	<option value="twice weekly,  8 weeks + unif">twice weekly,  8 weeks + unif </option>
+	</select> 	
+	</td>
+	</tr>
+</table>
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" height="60px" width="200px" style="padding: 5px 75px 0px 75px;">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+</div>	
+
+
+	
+	</section>
+	<footer>
+		<div class="centro">
+		<p class="pendereco"> Sponsors:</p>
+		<ul>
+		<li><a href="http://www.classicpizza1.com/" target="_blank"><img src="logop1.png" id="logop1"></a></li>
+		<li><a href="http://www.chevrolet.com/" target="_blank"><img src="logop3.jpg" id="logop3"></a></li>
+		<li><a href="http://jumpinwithus.net/" target="_blank"><img src="logop2.png" id="logop2"></a></li>
+		</ul>
+		</div>
+		
+		<div class="endereco">
+
+		<p class="pendereco">&reg 2015 Grêmio USA | Hudson Portuguese Club: 13 Port St, Hudson, MA 01749, USA | 
+33 Union Ave, Sudbury, MA 01776, USA - Contact number: (508) 9339124. &copy Grêmio USA - All rights reserved.</p>
+		<p class="pendereco">Developed by: &copy <a href="https://br.linkedin.com/pub/diogo-barreto/58/b09/949" target="_blank;" style="color:#fff"> Diogo Barreto</a>
+		
+		</div>
+		
+	</footer>
+	
+	</div>
+</body>
+</html>
