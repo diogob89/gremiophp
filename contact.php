f472f158dc0ad94d7f901a85dea27941b63c5a32
Diogo Barreto <diogob1789@gmail.com>
2018-02-17 01:49:23 -0200

    criacao do footer.php

--- contact.php
+++ contact.php
@@ -51,7 +51,7 @@
 	</header>
 
 	<!--apenas o location.html sem include nav-->
-<?php include 'nav.php'; ?>
+	<?php include 'nav.php'; ?>
 	
 	<section class = "centro">
 
@@ -61,25 +61,9 @@
 	<h2><a href="https://twitter.com/gremio_usa" target="_blank" style="color:#00adf1">Twitter</h2></a>
 	
 	</section>
-	<footer>
-		<div class="centro">
-		<p class="pendereco"> Sponsors:</p>
-		<ul>
-		<li><a href="http://www.classicpizza1.com/" target="_blank"><img src="logop1.png" id="logop1"></a></li>
-		<li><a href="http://www.chevrolet.com/" target="_blank"><img src="logop3.jpg" id="logop3"></a></li>
-		<li><a href="http://jumpinwithus.net/" target="_blank"><img src="logop2.png" id="logop2"></a></li>
-		</ul>
-		</div>
-		
-		<div class="endereco">
+	
 
-		<p class="pendereco">&reg 2015 Grêmio USA | Hudson Portuguese Club: 13 Port St, Hudson, MA 01749, USA | 
-33 Union Ave, Sudbury, MA 01776, USA - Contact number: (508) 9339124. &copy Grêmio USA - All rights reserved.</p>
-		<p class="pendereco">Developed by: &copy <a href="https://br.linkedin.com/pub/diogo-barreto/58/b09/949" target="_blank;" style="color:#fff"> Diogo Barreto</a>
-		
-		</div>
-		
-	</footer>
+	<?php include 'footer.php'; ?>
 	
 	</div>
 </body>
